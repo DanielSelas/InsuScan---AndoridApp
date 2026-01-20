f9b7f0906a0571eef337799feddecf4f738ae7a9
NimrodBar <131991393+NimiB2@users.noreply.github.com>
2026-01-20 23:53:00 +0200

    Update UI design, History

--- app/src/main/java/com/example/insuscan/history/MealHistoryItem.kt
+++ app/src/main/java/com/example/insuscan/history/MealHistoryItem.kt
@@ -4,5 +4,8 @@ data class MealHistoryItem(
     val title: String,
     val carbsText: String,
     val insulinText: String,
-    val timeText: String
+    val timeText: String,
+    val wasSickMode: Boolean = false,
+    val wasStressMode: Boolean = false,
+    val glucoseLevel: String? = null
 )
